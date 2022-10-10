784f0b3ea41c4bcf6397881131631e2598a2d9f4
Mourad <mouradface1998@gmail.com>
2022-10-10 22:08:20 +0100

    Fixes

---
+++ components/TheBreadCrumb.vue
@@ -0,0 +1,11 @@
+<template>
+    <div :class="['breadcrumb flex items-center justify-center text-[#8C8C8C] text-[10px] font-[Inter] leading-[13px] font-light', $attrs.class]">
+        <app-i name="fa6-solid:house" class="h-3 w-[14px]" />
+        <span class="mx-[10px]">&#62;</span>
+        <p>База новостроек</p>
+        <span class="mx-[10px]">&#62;</span>
+        <p>Аксай</p>
+        <span class="mx-[10px]">&#62;</span>
+        <p class="text-[#3478F6]">Квартал “Любимов 23”</p>
+    </div>
+</template>
