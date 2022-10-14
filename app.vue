1dac84ee706da89a563ee5778c7a81814350afe8
Mourad RL CADI <mouradface1998@gmail.com>
2022-10-14 16:31:59 +0100

    Test

--- app.vue
+++ app.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-import "@/assets/scss/index.scss";
+// import "@/assets/scss/index.scss";
 </script>
 
 <template>
-    <NuxtPage />
+    <!-- <NuxtPage /> -->
+
+    <div id="containerr">
+        <Counter :count="20" />
+    </div>
 </template>
+
+<style lang="scss" scoped>
+#containerr {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
